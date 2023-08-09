263a160b810fe995b94f4d8bb0ad087f4380d8c5
jessicahylee <jehyerinn@gmail.com>
2023-08-09 15:03:49 -0400

    Creating components for BrainFlix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navigation from './Components/Navigation/Navigation'
+import Hero from './Components/Hero/Hero'
+import Article from './Components/Article/Article'
 
 function App() {
   return (
     <>
       <Navigation />
+      <Hero />
+      <Article />
     </>
   )
 }
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div>
+      <video src=" https://project-2-api.herokuapp.com/stream?api_key=b9e93ea2-0a73-4a0e-8854-a727ea762866"></video>
+      <h1> BMX Rampage: 2021 Highlights</h1>
+      <hr></hr>
+      
+    </div>
+  )
+}
+
+export default Hero
