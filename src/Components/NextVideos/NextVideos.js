6e66d02d9fa6b237871ecc910f15bf07a8a40559
jessicahylee <jehyerinn@gmail.com>
2023-08-17 19:06:28 -0400

    Modify desktop mode

--- src/Components/NextVideos/NextVideos.js
+++ src/Components/NextVideos/NextVideos.js
@@ -3,18 +3,26 @@ import records from '../../Assets/Data/video-details.json'
 
 function NextVideos() {
   const newRecords = records.slice(1)
+  console.log('records')
+  const handleVideoClick = (videoUrl) => {
+    console.log(`Clicked video: ${videoUrl}`)
+  }
   return (
     <div className="inner">
       <h3 className="nextVideos"> NEXT VIDEOS </h3>
       {newRecords.map((record, idx) => (
-        <div className="box" key={idx}>
+        <div
+          onClick={() => handleVideoClick(records.video)}
+          className="box"
+          key={idx}
+        >
           <video
             className="videos_container"
             poster={record.image}
-            width="100%"
-            height="150px"
+            // width="100px"
+            // height="100px"
           >
-            {' '}
+            <source src={record.video} />
           </video>
           <div className="video-info">
             <h3 className="title_videos">{record.title}</h3>
